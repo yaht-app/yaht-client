a87470205b7577b2980437d39746e66e76d6b300
Sebastian Richner <srichner@outlook.com>
2021-04-05 15:03:20 +0200

    Add tailwind styles

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'tailwindcss/tailwind.css';
 
 Vue.config.productionTip = false;
 
