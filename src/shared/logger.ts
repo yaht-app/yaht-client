54dc344d420a9cd6f1db6598a0f5be0248268f77
Sebastian Richner <srichner@outlook.com>
2021-04-12 16:40:36 +0200

    Fix logger scope, clean up

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,21 +1,17 @@
 import { LOG_FILE_NAME, LOG_LEVEL } from '@/config/logger.config';
-import ElectronLog from 'electron-log';
+import { LogFunctions } from 'electron-log';
 import logger from 'electron-log';
 
-const getLogger = (
-  loggerName: string,
-  background = false
-): ElectronLog.LogFunctions => {
+const getLogger = (loggerName: string, background = false): LogFunctions => {
   const currentDate = new Date().toISOString().split('T')[0];
   const processName = background
     ? LOG_FILE_NAME.BACKGROUND
     : LOG_FILE_NAME.RENDERER;
 
-  logger.scope(loggerName);
   logger.transports.file.fileName = `${currentDate}-${processName}.log`;
   logger.transports.file.level = LOG_LEVEL.FILE;
   logger.transports.console.level = LOG_LEVEL.CONSOLE;
-  return logger;
+  return logger.scope(loggerName);
 };
 
 const logFile = logger.transports.file.getFile().path;
